c9a64a191861e43349f2708497859bd070b35460
TheFUlDeep <39480610+TheFUlDeep@users.noreply.github.com>
2020-10-22 19:04:06 +0300

    UpdateModelCallBack and UpdateWagonNumber

--- lua/autorun/metrostroi_717_clear_white_route.lua
+++ lua/autorun/metrostroi_717_clear_white_route.lua
@@ -12,6 +12,86 @@ local paramname = "Clear White"
 
 local tablename = "RouteNumberType"
 local readtablename = "Тип номера маршрута"
+
+if CLIENT then
+	MetrostroiWagNumUpdateRecieve = MetrostroiWagNumUpdateRecieve or function(index)
+		local ent = Entity(index)
+		--таймер, чтобы дождаться обновления сетевых значений (ну а вдруг)
+		timer.Simple(0.3,function()
+			if IsValid(ent) and ent.UpdateWagNumCallBack then 
+				ent:UpdateWagNumCallBack()
+				--ent:UpdateTextures()
+			end
+		end)
+	end
+end
+
+if SERVER then
+	local hooks = hook.GetTable()
+	if not hooks.MetrostroiSpawnerUpdate or not hooks.MetrostroiSpawnerUpdate["Call hook on clientside"] then
+		hook.Add("MetrostroiSpawnerUpdate","Call hook on clientside",function(ent)
+			if not IsValid(ent) then return end
+			local idx = ent:EntIndex()
+			for _,ply in pairs(player.GetHumans())do
+				if IsValid(ply)then ply:SendLua("MetrostroiWagNumUpdateRecieve("..idx..")")end
+			end
+		end)
+	end
+end
+
+local function RemoveEnt(wag,prop)
+	local ent = wag.ClientEnts and wag.ClientEnts[prop]
+	if IsValid(ent) then SafeRemoveEntity(ent)end
+end
+
+local function UpdateModelCallBack(ENT,cprop,callback,precallback)	
+	local modelcallback
+	if not ENT.UpdateWagNumCallBack then
+		function ENT:UpdateWagNumCallBack()end
+	end
+	
+	if modelcallback then
+		local oldmodelcallback = ENT.ClientProps[cprop].modelcallback or function() end
+		ENT.ClientProps[cprop].modelcallback = function(wag,...)
+			return modelcallback(wag) or oldmodelcallback(wag,...)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+	
+	if precallback then
+		local oldcallback = ENT.ClientProps[cprop].callback or function() end
+		ENT.ClientProps[cprop].callback = function(wag,cent,...)
+			precallback(wag,cent)
+			oldcallback(wag,cent,...)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+	
+	if callback then
+		local oldcallback = ENT.ClientProps[cprop].callback or function() end
+		ENT.ClientProps[cprop].callback = function(wag,cent,...)
+			oldcallback(wag,cent,...)
+			callback(wag,cent)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+end
+
 hook.Add("InitPostEntity","Metrostroi 717_mvm white route",function()
     local ENT = scripted_ents.GetStored(nomerogg.."_custom")
     if ENT then ENT = ENT.t else return end
@@ -29,26 +109,7 @@ hook.Add("InitPostEntity","Metrostroi 717_mvm white route",function()
 		inserted_index = table.insert(ENT.Spawner[foundtable][4],paramname)
 	end
 	
-	if SERVER then return end
-	
-	local function RemoveEnt(ent)if ent then SafeRemoveEntity(ent) end end
-	
-	local function UpdateModelCallBack(ENT,cprop,callback)
-		local oldcallback = ENT.ClientProps[cprop].callback or function() end
-		ENT.ClientProps[cprop].callback = function(wag,...)
-			oldcallback(wag,...)
-			callback(wag,...)
-		end
-		
-		--удаление пропа при апдейте спавнером для принудительного обновленяи модели
-		local oldupdate = ENT.UpdateWagonNumber or function() end
-		ENT.UpdateWagonNumber = function(wag,...)
-			RemoveEnt(wag.ClientEnts[cprop])
-			oldupdate(wag,...)
-		end
-
-	end
-	
+	if SERVER then return end	
 	
 	matspath = matspath:sub(11)
 	local defaultmatspath = "models/metrostroi_train/common/routes/ezh/m"
